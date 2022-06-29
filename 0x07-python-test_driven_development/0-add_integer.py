09962b49b15966e3077b98878c5d3dbb125bfb7a
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-06-29 14:21:35 +0300

    test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This module contains calculation functions"""
 
-
 def add_integer(a, b=98):
     """"This function does the addition of 2 arguments
     args:
@@ -19,5 +18,4 @@ def add_integer(a, b=98):
         a = int(a)
     if type(b) is float:
         b = int(b)
-
-    return a + b
+        return a + b
