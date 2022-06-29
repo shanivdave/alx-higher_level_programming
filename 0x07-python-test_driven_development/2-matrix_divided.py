cc406cabbbcf43264e2f968091eba26a076ae6e1
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-06-29 14:08:54 +0300

    test

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
 """
-    divides all elements of a matrix
+This is the "2-matrix_divided" module.
+The 2-matrix_divided module supplies one function, matrix_divided(matrix, div).
 """
 
 
 def matrix_divided(matrix, div):
-    """
-        forms a new matrix after all elements are divided
-    """
-
-    new_matrix = []
-    if type(div) not in [int, float]:
+    """Divides all elements in the matrix by div"""
+    if type(matrix) is not list:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    size = None
+    for l in matrix:
+        if type(l) is not list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+        if size is None:
+            size = len(l)
+        elif size != len(l):
+            raise TypeError("Each row of the matrix must have the same size")
+        for i in l:
+            if type(i) is not int and type(i) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    for row in matrix:
-        if len(row) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
-        transpose = []
-        for elem in row:
-            if type(elem) not in [int, float]:
-                raise TypeError("matrix must be a matrix (list of lists)\
- of integers/floats")
-            elem = round(elem / div, 2)
-            transpose.append(elem)
-        new_matrix.append(transpose)
-    return 
+    return [[round(i / div, 2) for i in l] for l in matrix]
