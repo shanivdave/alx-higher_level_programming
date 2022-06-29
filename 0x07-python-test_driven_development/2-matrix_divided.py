17e1351314922ed41443e5bbbe668728013c8e54
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-06-29 14:06:34 +0300

    test

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,36 +1,28 @@
 #!/usr/bin/python3
-"""Module for matrix Calculations"""
+"""
+    divides all elements of a matrix
+"""
 
 
 def matrix_divided(matrix, div):
-    '''This function devides all elements of a matrix
-    args:
-        matrix (list): list of lists of integers/floats
-        div (int): integer or float
-    returns:
-        a new matrix
-    '''
-    if type(div) is not int and type(div) is not float:
-        raise TypeError('div must be a number')
-    elif div == 0:
-        raise ZeroDivisionError('division by zero')
+    """
+        forms a new matrix after all elements are divided
+    """
 
-    fst_lst_len = len(matrix[0])
-    new_matrix = list()
-
-    for lst in matrix:
-        if len(lst) != fst_lst_len:
-            raise TypeError('Each row of the matrix must have the same size')
-
-        new_list = list()
-
-        for e in lst:
-            if type(e) is not int and type(e) is not float:
-                raise TypeError(
-                    'matrix must be a matrix(list of lists) of integers/floats'
-                    )
-            new_list.append(round(e / div, 2))
-
-        new_matrix.append(new_list)
-
-    return new_matrix
+    new_matrix = []
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    for row in matrix:
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        transpose = []
+        for elem in row:
+            if type(elem) not in [int, float]:
+                raise TypeError("matrix must be a matrix (list of lists)\
+ of integers/floats")
+            elem = round(elem / div, 2)
+            transpose.append(elem)
+        new_matrix.append(transpose)
+    return 
