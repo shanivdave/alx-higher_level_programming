b6f54e02490b8398e5c20bb6c5243e2f5a272144
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-06-29 21:53:49 +0300

    test

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-'''this module do stuff to strings'''
+"""
+This is the "5-test_indentation" module.
+The 5-text_indentation module supplies one function, text_indentation(text).
+"""
 
 
 def text_indentation(text):
-    '''this function fixes text indentation
-    args:
-        text (str): text to fix
-    returns:
-        None
-    '''
-
+    """splits a text into lines along "?", ":", "." followed by 2 new lines"""
     if type(text) is not str:
-        raise TypeError('text must be a string')
-
-    fst_idx = 0
-    for idx, char in enumerate(text):
-        if char == '.' or char == '?' or char == ':':
-            if (idx + 1) == len(text):
-                tmp_str = text[fst_idx:]
+        raise TypeError("text must be a string")
+    flag = 0
+    for a in text:
+        if flag == 0:
+            if a == ' ':
+                continue
             else:
-                tmp_str = text[fst_idx:idx + 1]
-            fst_idx = idx + 1
-            print(tmp_str.strip())
-            print()
-            continue
-
-        if idx == len(text) - 1:
-            tmp_str = text[fst_idx:]
-            print(tmp_str.strip())
+                flag = 1
+        if flag == 1:
+            if a == '?' or a == '.' or a == ':':
+                print(a)
+                print()
+                flag = 0
+            else:
+                print(a, end="")
