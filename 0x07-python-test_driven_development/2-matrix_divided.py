f41d43173cf6777f12e3e05a2a61f7c8a30a48a5
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-06-29 22:41:25 +0300

    test

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,7 +4,6 @@ This is the "2-matrix_divided" module.
 The 2-matrix_divided module supplies one function, matrix_divided(matrix, div).
 """
 
-
 def matrix_divided(matrix, div):
     """Divides all elements in the matrix by div"""
     if type(matrix) is not list:
