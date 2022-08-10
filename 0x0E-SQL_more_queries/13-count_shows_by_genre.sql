2411e1e7df4d3b9fae0c1db44ef1458c88424c4d
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-08-10 12:47:25 +0300

    more

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,4 +5,3 @@ SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
        ON tv_genres.id = tv_show_genres.genre_id
        GROUP BY tv_genres.name
        ORDER BY number_shows DESC;
-
