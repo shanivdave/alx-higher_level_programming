d7de91d0142a1ae60675edd8bfe0465d8d100587
shanivdave <davesarah77@gmail.com>
2022-09-06 03:52:16 -0700

    scope

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
-// Reverses a list
 
 exports.esrever = function (list) {
-  let left = 0;
-  let right = list.length - 1;
-  while (left <= right) {
-    let temp = list[right];
-    list[right] = list[left];
-    list[left] = temp;
-    left++;
-    right--;
-  }
-  return (list);
+	  const rev = [];
+	  for (const el in list) {
+		      rev.unshift(list[el]);
+		    }
+	  return rev;
 };
