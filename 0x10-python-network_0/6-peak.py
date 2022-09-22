de71ed7d4fe77d36310780098dd982c576eab260
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-09-22 16:38:00 +0300

    network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
-"""
-An element in the list is said to be peak if
-it is NOT smaller than its neighbors.
-For corner elements, we need to consider only one neighbor.
+""" Finds a peak in a list of unsorted integers
+    A peak is an Element in An array if its NOT smaller
+    than its neighbours.
 """
 
 
 def find_peak(list_of_integers):
-    """find pick element"""
+    """ returns the a peak in a an unsored list"""
+
     if list_of_integers == []:
         return None
 
-    def recursive(A, left=0, right=len(list_of_integers) - 1):
-        """helper recursive function"""
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
 
-        mid = (left + right) // 2
-
-        # check if the middle element is greater than its neighbors
-        if ((mid == 0 or list_of_integersA[mid - 1] <= list_of_integers[mid]) and
-                (mid == len(list_of_integers) - 1 or list_of_integersA[mid + 1] <= list_of_integers[mid])):
-            return A[mid]
-
-        # If the left neighbor of `mid` is greater than the middle element,
-        # find the peak recursively in the left sublist
-        if mid - 1 >= 0 and list_of_integersA[mid - 1] > list_of_integers[mid]:
-            return recursive(list_of_integers, left, mid - 1)
-
-        # If the right neighbor of `mid` is greater than the middle element,
-        # find the peak recursively in the right sublist
-        return recursive(list_of_integers, mid + 1, right)
-
-    return recursive(list_of_integers)
+    half = int(size / 2)
+    peak = list_of_integers[half]
+    if peak > list_of_integers[half - 1] and peak > list_of_integers[half + 1]:
+        return peak
+    elif peak < list_of_integers[half - 1]:
+        return find_peak(list_of_integers[:half])
+    else:
+        return find_peak(list_of_integers[half + 1:])
