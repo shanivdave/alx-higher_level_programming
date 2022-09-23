94881e6d4a0ca412e3f7dadacf01623c6c85d7ee
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-09-23 14:43:46 +0300

    web

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""Use requests package to make a get request to given URL
-"""
-import requests
-
+"""Fetches https://alx-intranet.hbtn.io/status"""
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
+    import requests
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    response = req.text
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(response)))
+    print("\t- content: {}".format(response))
