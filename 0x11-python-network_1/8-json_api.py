e7bf22577c607a101d203139b06e7c8e62e91b0a
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-09-23 13:34:10 +0300

    web1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,22 +1,28 @@
-#!/usr/bin/python3
-"""
-This Python script takes in a letter and sends a POST request
-to http://0.0.0.0:5000/search_user with the letter as a parameter
-"""
-import requests
-import sys
 
-if __name__ == "__main__":
-    url = 'http://0.0.0.0:5000/search_user'
-    if len(sys.argv) != 1:
-        q = sys.argv[1]
-    else:
-        q = ""
+#!/usr/bin/python3
+
+"""
+Python script that takes in a letter and sends a
+POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+    url = "http://0.0.0.0:5000/search_user"
     try:
-        req = requests.post(url, data={'q': q}).json()
-        if len(req) == 0:
-            print('No result')
-        else:
-            print('[{}] {}'.format(req.get('id'), req.get('name')))
-    except:
-        print('Not a valid JSON')
+        ar = sys.argv[1]
+    except Exception:
+        ar = ""
+    q = {"q": ar}
+    r = requests.post(url, data=q)
+    try:
+        result = r.json()
+    except Exception:
+        print("Not a valid JSON")
+        exit()
+    try:
+        print("[{}] {}".format(result['id'], result['name']))
+    except Exception:
+        print("No result")
