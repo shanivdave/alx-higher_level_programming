7fd1cfb3af2dcf359b976a169fc52dd1b8b987b0
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-09-23 14:29:22 +0300

    web1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """
-Script that fetches https://intranet.hbtn.io/status.
+fetches https://intranet.hbtn.io/status
 """
-import urllib.request
-
-
 if __name__ == "__main__":
-    request = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(request) as response:
-        html = response.read()
+    import urllib.request as request
+    with request.urlopen('https://intranet.hbtn.io/status') as r:
+        html = r.read()
         print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
