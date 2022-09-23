17e5b0f8130e8769fdabaff6c3a3e5c6d9efa54c
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-09-23 13:13:36 +0300

    web1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 """
-    A script that fetches https://intranet.htbn.io/status
+Fetches https://intranet.hbtn.io/status
+use the package urllib
+ body of the response must be displayed in tabulation before -
 """
-
-
 import urllib.request
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
 
-
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-        html = resp.read()
-
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf8')))
+print('Body response:\n\t- type: {}'.format(type(html)))
+print('\t- content: {}'.format(html))
