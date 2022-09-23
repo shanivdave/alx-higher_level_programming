ea901891b03ce54bb78f7ebb9ee10a3e35abbb45
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-09-23 14:35:59 +0300

    web

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
-Script that fetches https://intranet.hbtn.io/status
-You must use the package requests.
-"""
+# Use requests package to make a get request to given URL
 import requests
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
+    url = "https://intranet.hbtn.io/status"
     r = requests.get(url)
-    print('Body response:')
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
