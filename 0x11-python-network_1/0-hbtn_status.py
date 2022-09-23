30ccec33e9aabadc98e84deb0d94192066c14d50
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-09-23 13:18:27 +0300

    web1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """
-Fetches https://intranet.hbtn.io/status
-use the package urllib
- body of the response must be displayed in tabulation before -
+fetch holberton /statue page
 """
-import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
 
-print('Body response:\n\t- type: {}'.format(type(html)))
-print('\t- content: {}'.format(html))
+if __name__ == '__main__':
+    import urllib.request
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(html.__class__))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('ascii')))
