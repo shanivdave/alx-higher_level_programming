5a88d6083934507bd14aa206afffa85bd9dd7ae5
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-09-23 14:30:50 +0300

    web

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-'''
-Script that list 10 commits from the most recent to oldest of the repository
-rails by the user rails
-'''
-
-if __name__ == "__main__":
+"""
+list 10 commits (from the most recent to oldest) of the repository and user
+sent in as arguments
+"""
+if __name__ == '__main__':
+    import requests
     from sys import argv
-    from requests import get
-    repo = argv[1]
-    owner = argv[2]
-    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
-
-    req = get(url)
-
-    for i in range(10):
-        print("{sha}: {author[login]}".format(**req.json()[i]))
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(argv[2], argv[1]))
+    commits = r.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
