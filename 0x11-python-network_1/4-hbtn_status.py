a472c43a50a1af880da10536f2772cf4b583af6e
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-09-23 14:33:56 +0300

    web1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """
-fetches https://intranet.hbtn.io/status
+Script that fetches https://intranet.hbtn.io/status
+You must use the package requests.
 """
-if __name__ == '__main__':
-    import requests
-    r = requests.get('https://intranet.hbtn.io/status')
-    text = r.text
-    print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    r = requests.get(url)
+    print('Body response:')
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
